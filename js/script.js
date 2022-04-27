914339befe65de906f3c9efd16d2df08e6497cf8
GioCri01 <giovanni.crispino94@icloud.com>
2022-04-27 17:10:23 +0200

    fine bonus

--- js/script.js
+++ js/script.js
@@ -80,6 +80,39 @@ function form() {
     const nuovoMembroNome = document.getElementById("name").value;
     const ruoloMembro = document.getElementById("role").value;
     const imgMembro = document.getElementById("image").value;
+
+
+    const NuovoMembro = {
+        nome : nuovoMembroNome,
+        ruolo : ruoloMembro,
+        foto : imgMembro,
+
+    }
+
+    arrTeam.push(NuovoMembro);
+
+    const output=
+    `<div class="team-card">
+    <div class="card-image">
+      <img
+        src=${NuovoMembro.foto}
+        alt="Wayne Barnett"
+      />
+    </div>
+    <div class="card-text">
+      <h3>${NuovoMembro.nome}</h3>
+      <p>${NuovoMembro.ruolo}</p>
+    </div>
+    </div>
+
+    `
+    document.querySelector(".team-container").innerHTML += output;
+
+    
+
+    
+
+    
     
 }
 
